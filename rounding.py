72057b44f6c085f166108b3e5bd2f582ab0a5046
Ubuntu <ubuntu@ip-172-31-5-27.ec2.internal>
2021-01-11 02:05:25 +0000

    rounding.py

---
+++ rounding.py
@@ -0,0 +1,29 @@
+# !/usr/bin/env python3
+
+# Created by: Mohammad-al-buraiki
+# Created on January 2021
+# This program is for rounding numbers
+
+
+def rounding(user_input, times):
+    # pass by referrance
+    user_input[0] = user_input[0]*10**times[0]
+    user_input[0] = user_input[0] + 0.5
+    user_input[0] = user_input[0]*10**times[0]
+
+
+def main():
+    number1 = []
+    number2 = []
+    
+    user_input = input("Enter the number:")
+    times = input("Enter how many places you want to round:")
+    number1.append(user_input)
+    number2.append(times)
+    rounding(number1, number2)
+    print("{0}.".format(rounding))
+
+
+
+if __name__ == "__main__":
+    main()
