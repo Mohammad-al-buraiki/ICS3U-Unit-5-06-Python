546d5429a66f74b29a39bab218729aa07eae5fcc
Ubuntu <ubuntu@ip-172-31-5-27.ec2.internal>
2021-01-13 04:10:10 +0000

    rounding.py

--- rounding.py
+++ rounding.py
@@ -9,20 +9,20 @@ def rounding(user_input, times):
     # pass by referrance
     user_input[0] = user_input[0]*10**times[0]
     user_input[0] = user_input[0] + 0.5
-    user_input[0] = user_input[0]*10**times[0]
+    user_input[0] = int(user_input[0])
+    user_input[0] = user_input[0]/10**times[0]
 
 
 def main():
     number1 = []
     number2 = []
-    
-    user_input = input("Enter the number:")
-    times = input("Enter how many places you want to round:")
+
+    user_input = float(input("Enter the number:"))
+    times = int(input("Enter how many places you want to round:"))
     number1.append(user_input)
     number2.append(times)
     rounding(number1, number2)
-    print("{0}.".format(rounding))
-
+    print("{0}.".format(number1[0]))
 
 
 if __name__ == "__main__":
